6510050ef053ec0b328d87121ba5aef24d28e7e2
d4vyncy <davidserrudo@gmail.com>
2025-03-13 14:06:44 +0000

    counter

---
+++ Angular/02-bases/src/app/pages/counter/counter-page.component.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+
+@Component({
+    templateUrl: './counter-page.component.html',
+    styleUrl: './counter-page.component.css',
+})
+
+export class CounterPageComponent {
+
+    counter = 10;
+
+    increaseBy(value: number) {
+        this.counter += value;
+    }
+    resetCounter() {
+        this.counter = 10;
+    }
+}
